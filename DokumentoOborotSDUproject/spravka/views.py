50a63f8828712dbf9394bff6bf84dc6549b44224
Surkov <surkov.danilaa@mail.ru>
2025-06-20 15:22:14 +0300

    Delete DokumentoOborotSDUproject directory

--- DokumentoOborotSDUproject/spravka/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def glavnaya(request):
-    return HttpResponse('test')
-
-def neglavnaya(request):
-    return render(request, 'index.html')
